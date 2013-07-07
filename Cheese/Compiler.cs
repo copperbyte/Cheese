bf3ffb29cbf1d033eda84971d9ebb317f41b8dba
copperbyte <copper.byte@gmail.com>
2013-07-06 23:23:02 -0400

    So many Call fixes.
    Can't do these things without full LVal and RVal indo

--- Cheese/Compiler.cs
+++ Cheese/Compiler.cs
@@ -64,12 +64,13 @@ namespace Cheese
 
 		internal OP Code;
 		internal int A, B, C;
+		internal bool isA = false, isB=false, isC=false;
 
 		public Instruction() { ; }
 		public Instruction(OP code) { Code = code; }
-		public Instruction(OP code, int a) { Code = code; A = a; }
-		public Instruction(OP code, int a, int b) { Code = code; A = a; B = b;}
-		public Instruction(OP code, int a, int b, int c) { Code = code; A = a; B = b; C = c; }
+		public Instruction(OP code, int a) { Code = code; A = a; isA = true; }
+		public Instruction(OP code, int a, int b) { Code = code; A = a; B = b; isA = isB = true;}
+		public Instruction(OP code, int a, int b, int c) { Code = code; A = a; B = b; C = c; isA = isB = isC = true; }
 
 
 	}
@@ -106,6 +107,11 @@ namespace Cheese
 			this.Index = Index; 
 			this.Loc = Loc;
 			this.Side = Side;
+		}
+
+		public override string ToString()
+		{
+			return Side + ":" + Loc + ":" + Index.ToString();
 		}
 	}
 
@@ -163,7 +169,10 @@ namespace Cheese
 
 		internal void PrintInstructions() {
 			foreach(Instruction Inst in Instructions) {
-				Console.WriteLine("IC  {0}  {1}  {2}  {3}", Inst.Code, Inst.A, Inst.B, Inst.C);
+				Console.WriteLine("IC  {0}\t{1}  {2}  {3}", Inst.Code, 
+				                  (Inst.isA?Inst.A.ToString():""), 
+				                  (Inst.isB?Inst.B.ToString():""), 
+				                  (Inst.isC?Inst.C.ToString():""));
 			}
 		}
 	}
@@ -235,6 +244,33 @@ namespace Cheese
 				CurrFunc.UsedRegs.Remove(Reg.Index);
 		}
 
+
+		bool IsConsecutiveRegisterFree(int First, int Count) {
+			for(int I = 0; I < Count; I++) {
+				int CR = First + I;
+				if(CurrFunc.UsedRegs.Contains(CR))
+					return false;
+			}
+			return true;
+		}
+
+		// First is first tried, not promised first
+		VList GetConsecutiveRegisters(int First, int Count) {
+			while(true) {
+				if(IsConsecutiveRegisterFree(First, Count)) {
+					break;
+				}
+				First++;
+			}
+			VList Result = new VList();
+			for(int I = 0; I < Count; I++) {
+				int CR = First+I;
+				CurrFunc.UsedRegs.Add(CR);
+				Result.Add(new Value(CR, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+			}
+			return Result;
+		}
+
 		Value GetConstIndex(string ConstValue) {
 			foreach(ConstEntry Entry in CurrFunc.ConstantTable) {
 				if(Entry.StringVal != null && Entry.StringVal == ConstValue) {
@@ -290,6 +326,20 @@ namespace Cheese
 		}
 
 
+		Value GetLRegorTReg(VList LVals = null, VList RVals = null) {
+			Value Result = null;
+			if(LVals != null && RVals != null && LVals.Count > RVals.Count) {
+				if (LVals[RVals.Count].Loc == Value.ELoc.REGISTER ||
+				    LVals[RVals.Count].Loc == Value.ELoc.LOCAL) {
+					Result = LVals[RVals.Count];
+				}
+			}
+			if(Result == null)
+				Result = new Value(GetFreeRegister(), Value.ELoc.REGISTER, Value.ESide.RIGHT);
+			return Result;
+		}
+
+
 		void CompileBlock(ParseNode Block) {
 			if(Block.Children == null)
 				return;
@@ -327,7 +377,7 @@ namespace Cheese
 			Right = Assignment.Children[2];
 
 			VList LVs = CompileLeftVarList(Left);
-			VList RVs = CompileExpList(Right);
+			VList RVs = CompileExpList(Right, LVs);
 
 			int MaxCount = Math.Max(LVs.Count, RVs.Count);
 			for(int I = 0; I < MaxCount; I++) {
@@ -372,7 +422,7 @@ namespace Cheese
 			// Left is a NAME_LIST for local, not a VAR_LIST
 			VList LVs = CompileNameList(Left);
 
-			VList RVs = CompileExpList(Right);
+			VList RVs = CompileExpList(Right, LVs);
 
 			int MaxCount = Math.Max(LVs.Count, RVs.Count);
 			for(int I = 0; I < MaxCount; I++) {
@@ -555,90 +605,85 @@ namespace Cheese
 			return LVs;
 		}
 
-		VList CompileExpList(ParseNode ExpList) {
+		VList CompileExpList(ParseNode ExpList, VList LVals = null) {
+			// explist1 : (exp ',')* exp;
 			if(ExpList.Children == null)
 				return null;
 
-			VList Result = new VList(8);
+			VList Result = new VList();
 
 			foreach(ParseNode Child in ExpList.Children) {
 				if(Child.Type != ParseNode.EType.EXP)
 					continue;
 
-				VList CurrVs = CompileExp(Child);
+				VList CurrVs = CompileExp(Child, LVals, Result);
 				Result.AddRange(CurrVs);
 			}
 
 			return Result;
 		}
 
-		VList CompileExp(ParseNode Exp) {
+		VList CompileExp(ParseNode Exp, VList LVals = null, VList RVals = null) {
 
 			// sometimes CompileExp will be called, but Exp will actually 
 			//  be a node right under an exp, 
+			// So rather than every calling point being complicated, this will fix it up
 			// FIXME (wrap and call on fake-parent?)
 			if(Exp.Type != ParseNode.EType.EXP) {
 				ParseNode Temp = new ParseNode(ParseNode.EType.EXP);
 				Temp.Add(Exp);
-				return CompileExp(Temp);
+				return CompileExp(Temp, LVals, RVals);
 			}
-			//if(Exp.Type == ParseNode.EType.PREFIX_EXP)
-			//	return CompilePrefixExp(Exp);
-			//else if(Exp.Type == ParseNode.EType.UN_OP_WRAP) 
-			//	return new VList() { CompileUnOp(Exp) };
 
-			VList Result = new VList(2);
+			VList Result = new VList();
 
 			foreach(ParseNode Child in Exp.Children) {
 				if(Child.Type == ParseNode.EType.TERMINAL) {
 					if(Child.Token.Type == Token.EType.KEYWORD) {
 						if(Child.Token.IsKeyword("nil")) {
-							int DReg = GetFreeRegister();
-							CurrFunc.Instructions.Add(Instruction.OP.LOADNIL, DReg, DReg);
-							Result.Add(new Value(DReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+							Value Dest = GetLRegorTReg(LVals, RVals);
+							CurrFunc.Instructions.Add(Instruction.OP.LOADNIL, Dest.Index, Dest.Index);
+							Result.Add(Dest);
 						} else if(Child.Token.IsKeyword("true")) {
-							int DReg = GetFreeRegister();
-							CurrFunc.Instructions.Add(Instruction.OP.LOADBOOL, DReg, 1, 0);
-							Result.Add(new Value(DReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+							Value Dest = GetLRegorTReg(LVals, RVals);
+							CurrFunc.Instructions.Add(Instruction.OP.LOADBOOL, Dest.Index, 1, 0);
+							Result.Add(Dest);
 						} else if(Child.Token.IsKeyword("false")) {
-							int DReg = GetFreeRegister();
-							CurrFunc.Instructions.Add(Instruction.OP.LOADBOOL, DReg, 0, 0);
-							Result.Add(new Value(DReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+							Value Dest = GetLRegorTReg(LVals, RVals);
+							CurrFunc.Instructions.Add(Instruction.OP.LOADBOOL, Dest.Index, 0, 0);
+							Result.Add(Dest);
 						}
 
 
 					} else if(Child.Token.Type == Token.EType.NUMBER) {
 						double NumValue = 0.0;
 						Double.TryParse(Child.Token.Value, out NumValue);
-
 						Value CVal = GetConstIndex(NumValue);
-						int DReg = GetFreeRegister();
+						Value Dest = GetLRegorTReg(LVals, RVals);
+						CurrFunc.Instructions.Add(Instruction.OP.LOADK, Dest.Index, CVal.Index);
 
-						CurrFunc.Instructions.Add(Instruction.OP.LOADK, DReg, CVal.Index);
-
-						Result.Add(new Value(DReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+						Result.Add(Dest);
 					} else if(Child.Token.Type == Token.EType.STRING) {
 						Value CVal = GetConstIndex(Child.Token.Value);
-						int DReg = GetFreeRegister();
-
-						CurrFunc.Instructions.Add(Instruction.OP.LOADK, DReg, CVal.Index);
-
-						Result.Add(new Value(DReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
+						Value Dest = GetLRegorTReg(LVals, RVals);
+						CurrFunc.Instructions.Add(Instruction.OP.LOADK, Dest.Index, CVal.Index);
+						Result.Add(Dest);
 					}
 					// '...' ???
 				} else if(Child.Type == ParseNode.EType.UN_OP_WRAP) {
-					Result.Add(CompileUnOp(Child));
+					Result.Add(CompileUnOp(Child, LVals, RVals));
 				} else if(Child.Type == ParseNode.EType.BIN_OP_WRAP) {
-					Result.Add(CompileBinOp(Child));
+					Result.Add(CompileBinOp(Child, LVals, RVals));
 				} else if(Child.Type == ParseNode.EType.PREFIX_EXP) {
-					Result.AddRange(CompilePrefixExp(Child));
+					// might be a var, might be a function call
+					Result.AddRange(CompilePrefixExp(Child, LVals, RVals));
 				}
 			}
 
 			return Result;
 		}
 
-		VList CompilePrefixExp(ParseNode PrefixExp) {
+		VList CompilePrefixExp(ParseNode PrefixExp, VList LVals, VList RVals) {
 			// prefixexp: varOrExp nameAndArgs*;
 			VList Result = new VList();
 
@@ -663,11 +708,11 @@ namespace Cheese
 				if(VarVal.Loc == Value.ELoc.REGISTER || VarVal.Loc == Value.ELoc.LOCAL) {
 					Result.Add(VarVal);
 				} else {
-					int DestReg = GetFreeRegister();
-					Result.Add(new Value(DestReg, Value.ELoc.REGISTER, Value.ESide.RIGHT));
-					CurrFunc.Instructions.Add(Instruction.OP.GETGLOBAL, DestReg, VarVal.Index);
+					Value Dest = GetLRegorTReg(LVals, RVals);
+					Result.Add(Dest);
+					CurrFunc.Instructions.Add(Instruction.OP.GETGLOBAL, Dest.Index, VarVal.Index);
 				}
-			} else { // assume ( exp )
+			} else { // assume ( exp ) 
 				ParseNode Exp = VarOrExp.Children[1];
 				Result = CompileExp(Exp);
 			}
@@ -686,32 +731,68 @@ namespace Cheese
 				if(NameAndArgs.Children[0].Type == ParseNode.EType.TERMINAL &&
 					NameAndArgs.Children[0].Token.IsOperator(":")) {
 					ClassFuncName = NameAndArgs.Children[1];
+					// OP.SELF 
 					Args = NameAndArgs.Children[2];
 				} else {
 					Args = NameAndArgs.Children[0];
 				}
 
-				VList ArgVs = CompileArgs(Args);
+				int RetCount = 0; 
+				if(LVals != null && RVals != null)
+					RetCount = LVals.Count - RVals.Count;
+				int ArgCount = CountArgs(Args);
 
-				// Set up the call?
+				int StackSpaceNeeded = Math.Max(RetCount, ArgCount+1);
+
+				Console.WriteLine(" R:{0}  A:{1}  SN:{2}", RetCount, ArgCount, StackSpaceNeeded);
+				Console.Write(" UR: ");
+				foreach(var R in CurrFunc.UsedRegs) {
+					Console.Write(" {0} ", R); 
+				}
+				Console.WriteLine();
+
 				Value FuncVal = Result[Result.Count - 1];
 				Result.RemoveAt(Result.Count - 1);
 
-				int RetCount = 0; // FIXME How do I know this? Count LVals? That isn't a parameter!!
+				VList StackSpace;
+				if(IsConsecutiveRegisterFree(FuncVal.Index + 1, StackSpaceNeeded - 1)) {
+					// Keep the stack by our func
+					StackSpace = GetConsecutiveRegisters(FuncVal.Index + 1, StackSpaceNeeded - 1);
+					StackSpace.Insert(0, FuncVal);
+				} else {
+					// Shuffle the FuncVal around to our used stack area
+					StackSpace = GetConsecutiveRegisters(FuncVal.Index, StackSpaceNeeded);
+					FreeRegister(FuncVal);
+					CurrFunc.Instructions.Add(Instruction.OP.MOVE, StackSpace[0].Index, FuncVal.Index);
+					FuncVal = StackSpace[0];
+				}
+				Console.WriteLine(" FV:{0} ", FuncVal.Index);
+
+				// check if FuncVal.Index + StackSpaceNeeded is avail, consecutive. 
+				//  else get a new reg at the top, move funcval to it.
+				//  Allocate LVals for args? Pass into CompileArgs?
+				VList ArgRegs = new VList();
+				for(int i =0; i < ArgCount; i++) 
+					ArgRegs.Add(StackSpace[i+1]);
+				VList ArgVs = CompileArgs(Args, ArgRegs);
+
 				CurrFunc.Instructions.Add(Instruction.OP.CALL, FuncVal.Index, ArgVs.Count+1, RetCount+1);
+				// Add return regs to Result?
+				for(int i = 0; i < RetCount; i++)
+					Result.Add(StackSpace[i]);
 			}
 
 			return Result;
 		}
 
-		VList CompileArgs(ParseNode Args) {
+		VList CompileArgs(ParseNode Args, VList LVals=null) {
 			// args :  '(' (explist1)? ')' | tableconstructor | string ;
 			VList Result = null;
 
 			if (Args.Children[0].Type == ParseNode.EType.TERMINAL &&
 				Args.Children[0].Token.IsBracket("(")) {
 				if(Args.Children[1].Type == ParseNode.EType.EXP_LIST) {
-					Result = CompileExpList(Args.Children[1]);
+					Result = CompileExpList(Args.Children[1], LVals);
 				}
 			} else if(Args.Children[0].Type == ParseNode.EType.TABLE_CONS) {
 				; // FIXME
@@ -723,7 +804,30 @@ namespace Cheese
 			return Result;
 		}
 
-		Value CompileBinOp(ParseNode BinOp) {
+		int CountArgs(ParseNode Args) {
+			// args :  '(' (explist1)? ')' | tableconstructor | string ;
+			int ArgCount = 0;
+
+			if (Args.Children[0].Type == ParseNode.EType.TERMINAL &&
+			    Args.Children[0].Token.IsBracket("(")) {
+				if(Args.Children[1].Type == ParseNode.EType.EXP_LIST) {
+					ParseNode ExpList = Args.Children[1];
+					foreach(ParseNode Exp in ExpList.Children) {
+						if(Exp.Type == ParseNode.EType.EXP)
+							ArgCount++;
+					}
+				}
+			} else if(Args.Children[0].Type == ParseNode.EType.TABLE_CONS) {
+				; // FIXME
+			} else if(Args.Children[0].Type == ParseNode.EType.TERMINAL &&
+			          Args.Children[0].Token.Type == Token.EType.STRING) {
+				; // FIXME
+			}
+
+			return ArgCount;
+		}
+
+		Value CompileBinOp(ParseNode BinOp, VList LVals = null, VList RVals = null) {
 
 			ParseNode Left = BinOp.Children[0];
 			ParseNode Op = BinOp.Children[1];
@@ -751,14 +855,15 @@ namespace Cheese
 			FreeRegister(LV[0]);
 			FreeRegister(RV[0]);
 
-			Value Result = new Value(GetFreeRegister(), Value.ELoc.REGISTER, Value.ESide.RIGHT);
+			//Value Result = new Value(GetFreeRegister(), Value.ELoc.REGISTER, Value.ESide.RIGHT);
+			Value Result = GetLRegorTReg(LVals, RVals);
 
 			CurrFunc.Instructions.Add(InstOp, Result.Index, LV[0].Index, RV[0].Index);
 
 			return Result;
 		}
 
-		Value CompileUnOp(ParseNode UnOp) {
+		Value CompileUnOp(ParseNode UnOp, VList LVals = null, VList RVals = null) {
 
 			ParseNode Op = UnOp.Children[0];
 			ParseNode Right = UnOp.Children[1];
@@ -776,7 +881,7 @@ namespace Cheese
 
 			FreeRegister(RV[0]);
 
-			Value Result = new Value(GetFreeRegister(), Value.ELoc.REGISTER, Value.ESide.RIGHT);
+			Value Result = GetLRegorTReg(LVals, RVals);
 
 			CurrFunc.Instructions.Add(InstOp, Result.Index, RV[0].Index);
 
