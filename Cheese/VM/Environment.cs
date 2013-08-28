4f001ee7c6fbb20d3f1020027751f434af87f677
copperbyte <copper.byte@gmail.com>
2013-08-28 00:36:07 -0400

    Start of VM

--- Cheese/VM/Environment.cs
+++ Cheese/VM/Environment.cs
@@ -1,12 +1,27 @@
 using System;
 
-namespace Cheese
+
+namespace Cheese.Machine
 {
 	public class Environment
 	{
+		internal Machine Machine;
+
+		// Globals?
+		// Upvals?
+
+
 		public Environment()
 		{
+			Machine = new Machine();
 		}
+
+
+		public void ExecuteChunk(Chunk Chunk) {
+
+			Machine.ExecuteChunk(Chunk);
+		}
+
 	}
 }
 
--- Cheese/VM/Machine.cs
+++ Cheese/VM/Machine.cs
@@ -1,12 +1,44 @@
 using System;
+using System.Collections.Generic;
 
-namespace Cheese
+using Cheese;
+
+
+namespace Cheese.Machine
 {
-	public class Machine
-	{
-		public Machine()
-		{
+
+
+
+
+	internal class VmStack {
+		//List<LuaValue> Storage;
+		int FramePointer;
+
+
+		internal VmStack() {
+			//Storage = new List<LuaValue>();
+			FramePointer = 0;
 		}
 	}
+
+	public class Machine
+	{
+		// Stack?
+		// Globals table, from Envrionment?
+		// Upval table?
+
+		public Machine()
+		{
+
+		}
+
+
+		internal void ExecuteChunk(Chunk Chunk) {
+			;//
+		}
+
+	}
+
+
 }
 
