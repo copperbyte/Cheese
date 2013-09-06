ce648b7e023a81c244b528e153358a97be37175d
copperbyte <copper.byte@gmail.com>
2013-09-05 23:44:20 -0400

    Added 'print', thing for builtin functions

--- Cheese/VM/Environment.cs
+++ Cheese/VM/Environment.cs
@@ -3,6 +3,26 @@ using System;
 
 namespace Cheese.Machine
 {
+
+	internal static class SystemFunctions {
+
+		internal static LuaValue print(LuaTable Arguments) {
+
+			bool First = true;
+			foreach(LuaValue Curr in Arguments.EnumerableArray) {
+				if(!First)
+					Console.Write("\t");
+				Console.Write(Curr.ToString());
+				First = false;
+			}
+
+			Console.WriteLine();
+
+			return LuaNil.Nil;
+		}
+
+	}
+
 	public class Environment
 	{
 		internal Machine Machine;
@@ -17,15 +37,26 @@ namespace Cheese.Machine
 		public Environment()
 		{
 			Globals = new LuaTable();
-			Machine = new Machine(Globals);
+
+			InitSystemFunctions();
 		}
 
 
 		public void ExecuteChunk(Chunk Chunk) {
 
+			if(Machine == null)
+				Machine = new Machine(Globals);		
+
 			Machine.ExecuteChunk(Chunk);
 		}
 
+
+		////
+		private void InitSystemFunctions() {
+
+			Globals[new LuaString("print")] = new LuaDelegate(SystemFunctions.print);
+
+		}
 	}
 }
 
