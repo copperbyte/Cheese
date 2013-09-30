61a140a55676545ed55327af72144fc75d167208
copperbyte <copper.byte@gmail.com>
2013-09-29 23:22:07 -0400

    Added TailCallTest

--- NUnitTests/Test.cs
+++ NUnitTests/Test.cs
@@ -205,6 +205,27 @@ namespace NUnitTests
 		}
 
 		[Test()]
+		public void TailCallTest()
+		{
+			string Code = @"
+				function sum2(accu, n)
+				  if n > 0 then
+				    accu.value = accu.value + n;
+				    return sum2(accu, n-1);
+				  end
+				end
+
+				local accu = {value = 0}
+				sum2(accu, 1000000)
+				print(accu.value) ";
+
+			string Expected = "500000500000\r\n";
+
+			ConsoleCompareTest(Code, Expected);
+		}
+
+
+		[Test()]
 		public void AssignCompTest()
 		{
 			string Code = @"
